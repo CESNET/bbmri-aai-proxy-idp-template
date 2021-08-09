4dbe6f1ea26d2b1518915a6027a076e532c57173
Dominik Frantisek Bucik <bucik@ics.muni.cz>
2021-08-09 14:18:09 +0200

    chore: use semantic release and ECS

--- themes/bbmri/default/includes/footer.php
+++ themes/bbmri/default/includes/footer.php
@@ -1,8 +1,8 @@
-<?php
+<?php declare(strict_types=1);
 
 use SimpleSAML\Module;
 
-if (!empty($this->data['htmlinject']['htmlContentPost'])) {
+if (! empty($this->data['htmlinject']['htmlContentPost'])) {
     foreach ($this->data['htmlinject']['htmlContentPost'] as $c) {
         echo $c;
     }
@@ -17,8 +17,8 @@ if (!empty($this->data['htmlinject']['htmlContentPost'])) {
 
         <div style="float: left;">
             <img src="<?php echo Module::getModuleUrl(
-                'bbmri/res/img/BBMRI-ERIC-gateway-for-health_216.png'
-            ) ?>">
+    'bbmri/res/img/BBMRI-ERIC-gateway-for-health_216.png'
+) ?>">
         </div>
 
         <div style="float: left;">
@@ -26,7 +26,7 @@ if (!empty($this->data['htmlinject']['htmlContentPost'])) {
                 &nbsp; &nbsp; +43 316 34 99 17-0 &nbsp;
                 <a href="mailto:contact@bbmri-eric.eu">contact@bbmri-eric.eu</a>
             </p>
-            <p>Copyright © BBMRI-ERIC <?php echo date("Y"); ?> </p>
+            <p>Copyright © BBMRI-ERIC <?php echo date('Y'); ?> </p>
         </div>
     </div>
 
